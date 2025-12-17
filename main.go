87920de956144c4f41f50fc7fe22d1b5b06ccad3
Jānis Lībeks <libeks@gmail.com>
2025-12-17 14:10:45 -0500

    Add more sophisticated arg parsing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"time"
@@ -10,18 +11,19 @@ import (
 	"github.com/libeks/go-plotter-svg/svg"
 )
 
+type Config struct {
+	fname     string
+	sceneName string
+	debug     bool
+}
+
 func main() {
 	args := os.Args[1:]
-	fname := "gallery/test.svg"
 	sizePx := 10000.0
-	sceneName := "test-density-v2"
 
-	if len(args) > 0 {
-		fname = args[0]
-		args = args[1:]
-	}
-	if len(args) > 0 {
-		sceneName = args[0]
+	config, err := parseFlags(args)
+	if err != nil {
+		panic(err)
 	}
 
 	outerBox := primitives.BBox{
@@ -31,7 +33,16 @@ func main() {
 	start := time.Now()
 	innerBox := outerBox.WithPadding(500) // enough to no hit the edges
 	library := scenes.GatherScenes()
-	sceneFn, err := library.Get(sceneName)
+	if config.debug {
+		sceneNames := library.GetNames()
+		fmt.Printf("These scenes are available:\n")
+		for _, name := range sceneNames {
+			fmt.Printf("\t%s\n", name)
+		}
+		return
+	}
+
+	sceneFn, err := library.Get(config.sceneName)
 	if err != nil {
 		panic(err)
 	}
@@ -39,10 +50,46 @@ func main() {
 
 	scene.CalculateStatistics()
 	svg.SVG{
-		Fname:    fname,
+		Fname:    config.fname,
 		Width:    "12in",
 		Height:   "9in",
 		Document: scene,
 	}.WriteSVG()
 	fmt.Printf("Rendering took %s.\n", time.Since(start))
+}
+
+func parseFlags(args []string) (Config, error) {
+	fname := "gallery/test.svg"
+	sceneName := "test-density-v2"
+	// n := len(args)
+	for len(args) > 0 {
+		arg := args[0]
+		if arg == "--fname" {
+			if len(args) < 2 {
+				return Config{}, errors.New("Parameter '--fname' must be followed by a filename")
+			}
+			fname = args[1]
+			args = args[2:]
+			continue
+		}
+		if arg == "--scene" {
+			if len(args) < 2 {
+				return Config{}, errors.New("Parameter '--scene' must be followed by a scene name")
+			}
+			sceneName = args[1]
+			args = args[2:]
+			continue
+		}
+		if arg == "--list-scenes" {
+			return Config{debug: true}, nil
+		}
+		if arg == "--help" {
+			//TODO: write a help doc ehre
+		}
+		return Config{}, errors.New(fmt.Sprintf("Not sure what to do with parameters %v", args))
+	}
+	return Config{
+		fname:     fname,
+		sceneName: sceneName,
+	}, nil
 }
---
+++ scenes/library.go
@@ -0,0 +1,43 @@
+package scenes
+
+import (
+	"errors"
+	"fmt"
+	"maps"
+	"slices"
+	"strings"
+
+	"github.com/libeks/go-plotter-svg/primitives"
+)
+
+func SceneLibrary() sceneLibrary {
+	return sceneLibrary{
+		scenes: make(map[string]func(b primitives.BBox) Document),
+	}
+}
+
+type sceneLibrary struct {
+	// map keys are considered to be case insensitive
+	scenes map[string]func(b primitives.BBox) Document
+}
+
+func (l *sceneLibrary) Add(name string, scene func(b primitives.BBox) Document) error {
+	lowerName := strings.ToLower(name)
+	if _, ok := l.scenes[lowerName]; ok {
+		return errors.New(fmt.Sprintf("Scene with name '%s' already added", name))
+	}
+	l.scenes[lowerName] = scene
+	return nil
+}
+
+func (l *sceneLibrary) Get(name string) (func(b primitives.BBox) Document, error) {
+	lowerName := strings.ToLower(name)
+	if scene, ok := l.scenes[lowerName]; ok {
+		return scene, nil
+	}
+	return nil, errors.New(fmt.Sprintf("Couldn't find scene with name '%s'", name))
+}
+
+func (l *sceneLibrary) GetNames() []string {
+	return slices.Collect(maps.Keys(l.scenes))
+}
